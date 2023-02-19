590dd3a7a3b401cb33f9daee4ff0cf0662a3d221
Pat Cavit <github@patcavit.com>
2023-02-19 15:54:17 -0800

    chore: types & readme match reality

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,8 @@ import type { Plugin } from 'rollup';
 export interface RollupPluginGlobSyncOptions {
     /**
      * Array of glob patterns to use for finding files to copy.
-     * @default []
      */
-    patterns?: string[];
+    globs: string[];
 
     /**
      * Directory to copy files into.
@@ -20,28 +19,22 @@ export interface RollupPluginGlobSyncOptions {
     dir?: string;
 
     /**
-     * Whether or not to remove all files within dist when rollup starts up.
+     * Whether or not to remove files within dist when rollup starts up, can be an array of glob patterns
      * @default true
      */
-    clean?: boolean;
-
-    /**
-     * Array of glob patterns to use for finding files to clean.
-     * @default dest
-     */
-    clean_globs?: [];
+    clean?: boolean | string[];
 
     /**
      * A (file) => file function that allows for programatically changing the destination of files.
      * @default identify
      */
-    transform?: [];
+    transform?: boolean | ((source: string) => string);
 
     /**
      * A string defining a package name for the manifest of files to be copied/watched that you can import in your code.
      * @default false
      */
-    manifest?: boolean;
+    manifest?: boolean | string;
 
     /**
      * A shorthand to enable verbose logging.
